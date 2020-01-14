7d43c0acb32a95f6a6f0ee64e8eb7dee348c729b
ryanyoung1989 <ryanyoung1989@hotmail.com>
2020-01-13 23:22:56 -0800

    fuck you

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class QuizQuestionButton extends Component {
 
     handleClick(buttonText) {
-        //this.props.handleClick(buttonText);
+
     }
 
     render() {
