1a219195bafb6b503c0840a348fb8c0c66b5325b
ryanyoung1989 <ryanyoung1989@hotmail.com>
2020-01-13 23:24:26 -0800

    un-ruining my fuckign code

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class QuizQuestionButton extends Component {
 
     handleClick(buttonText) {
-
+        this.props.handleClick(buttonText);
     }
 
     render() {
         return (
             <li>
-                <button onClick={ this.handleClick.bind(this) }>
+                <button onClick={ this.handleClick.bind(this, this.props.button_text) }>
                     { this.props.button_text }
                 </button> 
             </li>
